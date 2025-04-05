511659c509c009f02cf621715a212f860e1fa462
Anandita Cherian <anandita.cherian@student.manchester.ac.uk>
2025-04-05 17:54:22 +0100

    Login linked to main page

--- app.py
+++ app.py
@@ -60,7 +60,9 @@ def load_settings():
         config = json.load(f)
     return jsonify(config)
 
-
+@app.route("/main")
+def main_page():
+    return render_template("main.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
